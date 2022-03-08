9122a0c010a33c220453e67edc5ee764d5b0863d
Rikard 'klanton' Lundberg <rikard.lundberg@hotmail.com>
2022-03-08 12:45:27 +0100

    Forgot one file, Phone

---
+++ XMLParser/Parser/Fields/Phone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLParser
+{
+    class Phone
+    {
+        private string _landline, _mobile;
+
+        public Phone(string[] parts)
+        {
+            _landline = Constants.NotApplicable;
+            _mobile = Constants.NotApplicable;
+
+            try
+            {
+                _landline = parts[1];
+                _mobile = parts[2];
+            }
+            catch (Exception ex)
+            {
+                //
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = Constants.PhoneStart;
+
+            str += Constants.LandLineStart + _landline + Constants.LandLineEnd;
+
+            str += Constants.MobileStart + _mobile + Constants.MobileEnd;
+
+            str += Constants.PhoneEnd;
+
+            return str;
+        }
+    }
+}
